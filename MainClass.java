0fc42eaeb2db1ad6ab95d2055a93300303e85036
Mahmoud Ahmed <mahmoud.ahmed.ebrahim@fcis.asu.edu.eg>
2015-08-13 18:21:56 +0200

    Create MainClass.java

---
+++ MainClass.java
@@ -0,0 +1,12 @@
+package JavaPackage;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LCGclass obj=new LCGclass();
+		obj.getInput();
+		obj.getResult();
+	}
+
+}
